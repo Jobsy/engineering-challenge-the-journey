c64d4d1023400816bb53073596b175be2b5eaf5d
Oluwajoba Bello <oluwajobabello@gmail.com>
2023-07-23 23:05:47 +0100

    add page with 3*9 table

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Grid from '../components/Grid/Grid';
+
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Your Personal Staking Calculator</h1>
+      <Grid rows={9} columns={3} />
+    </div>
+  );
+};
+
+
+export default Home;
